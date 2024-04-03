0e24fa0e0d2adf238e604e9b3bad954d3e1b2ba7
Olivier Pi <olivier@opierre.fr>
2024-04-03 09:51:32 +0200

    fix: change picto hero

--- bedrock/web/app/themes/labeps-theme/resources/views/partials/hero.blade.php
+++ bedrock/web/app/themes/labeps-theme/resources/views/partials/hero.blade.php
@@ -1,9 +1,11 @@
-<div class="wp-block-group actualite-hero container mx-auto header-background">
-  <figure class="wp-block-image">
-    <img src="@asset('images/picto-hero.svg')" alt="" class="wp-image-115"/>
-  </figure>
-  <div class="block-heading-text">
-    <h1 class="wp-block-heading">{!! $title !!}</h1>
-    <p>{!! isset($pageIntro) ? $pageIntro : "" !!}</p>
-  </div>        
-</div>
+<section class="wp-block-group container mx-auto my-10">
+  <div class="actualite-hero flex">
+    <figure class="wp-block-image">
+      <img src="@asset('images/picto-hero.svg')" alt="Picto Lab" class=""/>
+    </figure>
+    <div class="block-heading-text w-8/12 md:w-auto">
+      <h1 class="">{!! $title !!}</h1>
+      <p class="w-fit">{!! isset($pageIntro) ? $pageIntro : "" !!}</p>
+    </div> 
+  </div>    
+</section>
