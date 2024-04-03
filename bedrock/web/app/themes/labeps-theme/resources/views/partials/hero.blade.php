0aaf77473d813d322ef14e4625f8dacec21f0548
Olivier Pi <olivier@opierre.fr>
2024-04-03 08:49:58 +0200

    fix: acf7 and bud config

--- bedrock/web/app/themes/labeps-theme/resources/views/partials/hero.blade.php
+++ bedrock/web/app/themes/labeps-theme/resources/views/partials/hero.blade.php
@@ -1,6 +1,6 @@
 <div class="wp-block-group actualite-hero container mx-auto header-background">
   <figure class="wp-block-image">
-    <img src="@asset('images/archive-hero.svg')" alt="" class="wp-image-115"/>
+    <img src="@asset('images/picto-hero.svg')" alt="" class="wp-image-115"/>
   </figure>
   <div class="block-heading-text">
     <h1 class="wp-block-heading">{!! $title !!}</h1>
