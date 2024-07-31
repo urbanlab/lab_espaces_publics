2fe5562578f3c3a16ef8ce9f7d04c4b8e29ab295
Olivier Pi <olivier@opierre.fr>
2024-07-31 08:32:39 +0200

    fix: style tab view

--- bedrock/web/app/themes/labeps-theme/resources/views/archive-projects.blade.php
+++ bedrock/web/app/themes/labeps-theme/resources/views/archive-projects.blade.php
@@ -15,14 +15,14 @@
     </x-alert>
     {!! get_search_form(false) !!}
   @endif
-
-  <section class="container mx-auto my-4">
-    <div id="filter-overlay" class="fixed inset-0 bg-black opacity-50 hidden lg:hidden"></div>
-
-    <div class="view-switcher">
+  <div class="view-switcher">
+    <div class="container mx-auto">
       <button id="list-view-button" class="active">List View</button>
       <button id="map-view-button">Map View</button>
     </div>
+  </div>
+  <section class="container mx-auto my-4">
+    <div id="filter-overlay" class="fixed inset-0 bg-black opacity-50 hidden lg:hidden"></div>
 
     <div class="flex flex-col lg:flex-row">
       <x-button id="filter-button" text="{{ __('FILTRER', 'labeps-theme') }}" class=" w-1/3 text-black border-2 border-black rounded-md p-2 mb-4 lg:hidden" icon="fas fa-filter" />
