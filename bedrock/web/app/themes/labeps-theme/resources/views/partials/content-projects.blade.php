2e37c5194a8bda11a79ba5550ac62d42fa9fd121
nochoa89 <65715658+nochoa89@users.noreply.github.com>
2025-11-20 12:24:49 +0100

    Nested links removed in article cards (#116)

--- bedrock/web/app/themes/labeps-theme/resources/views/partials/content-projects.blade.php
+++ bedrock/web/app/themes/labeps-theme/resources/views/partials/content-projects.blade.php
@@ -1,18 +1,16 @@
 <article id="post-{{ get_the_ID() }}" class="card self-stretch @php(get_post_class())">
-    <a href="{{ get_permalink() }}">
-      <figure>
-        {!!get_the_post_thumbnail()!!}
-      </figure>
-    </a>
+  <a href="{{ get_permalink() }}">
+    <figure>
+     {!!get_the_post_thumbnail()!!}
+    </figure>
     <div class="entry-summary md:p-4">
-        <h3 class="entry-title text-base! leading-7 py-1! font-bold! lg:text-lg! break-words">
-          <a href="{{ get_permalink() }}">
-            {!! $title !!}
-          </a>
-        </h3>
-        @foreach ((wp_get_post_terms($post->ID, 'commune')) as $item)
+      <h3 class="entry-title text-base! leading-7 py-1! font-bold! lg:text-lg! break-words">
+        {!! $title !!}
+      </h3>
+      @foreach ((wp_get_post_terms($post->ID, 'commune')) as $item)
         <p class="w-fit text-xs py-1 px-2 m-1 me-8">📍{{$item->name}}</p>
       @endforeach
         <p>{{the_excerpt()}}</p>
     </div>
-  </article>
+  </a>
+</article>
--- bedrock/web/app/themes/labeps-theme/resources/views/partials/content-ressources.blade.php
+++ bedrock/web/app/themes/labeps-theme/resources/views/partials/content-ressources.blade.php
@@ -3,15 +3,14 @@
         <figure>
             {!!get_the_post_thumbnail()!!}
         </figure>
-    </a>
-    <div class="entry-summary md:p-4">
-        @foreach ((wp_get_post_terms($post->ID, 'types')) as $item)
-            <p class="bg-secondary text-white w-fit text-xs! py-1.5! px-2! m-1! rounded-md! me-8!">{{$item->name}}</p>
-        @endforeach
-        <h3 class="entry-title text-base leading-7 p-1 font-bold lg:text-lg">
-            <a href="{{ get_permalink() }}">
+        <div class="entry-summary md:p-4">
+            @foreach ((wp_get_post_terms($post->ID, 'types')) as $item)
+                <p class="bg-secondary text-white w-fit text-xs! py-1.5! px-2! m-1! rounded-md! me-8!">{{$item->name}}</p>
+            @endforeach
+            <h3 class="entry-title text-base leading-7 p-1 font-bold lg:text-lg">
                 {!! $title !!}
-            </a>
-        </h3>
-    </div>
+                
+            </h3>
+        </div>
+    </a>    
 </article>
