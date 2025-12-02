1e12e1ab8be29d6dd3f1c0b5a5d00ce52530012e
bntdlb <94608233+bntdlb@users.noreply.github.com>
2025-12-02 12:35:24 +0100

    Hide base results if no results on post type archives pages (#127)

--- bedrock/web/app/themes/labeps-theme/resources/views/components/grid.blade.php
+++ bedrock/web/app/themes/labeps-theme/resources/views/components/grid.blade.php
@@ -1,14 +1,14 @@
-@if (! have_posts())
+@if (have_posts() === false)
     <x-alert type="warning">
         {!! __('Aucun résultat ne répond à votre recherche.', 'labeps-theme') !!}
     </x-alert>
+@else
+    <div>
+        <div id="results-container" class="grid {{$columnClasses}} gap-4">
+            {{$slot}}
+        </div>
+        <div id="pagination-container">
+            {!! the_posts_pagination(array('class' => 'list-none')) !!}
+        </div>
+    </div>
 @endif
-
-<div>
-    <div id="results-container" class="grid {{$columnClasses}} gap-4">
-        {{$slot}}
-    </div>
-    <div id="pagination-container">
-        {!! the_posts_pagination(array('class' => 'list-none')) !!}
-    </div>
-</div>
